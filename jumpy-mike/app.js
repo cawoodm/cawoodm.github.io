20f872b0bddec335946b7277f0472326917f67f1
Marc Cawood <cawoodm@gmail.com>
2017-08-22 12:35:31 +0200

    Release

--- jumpy-mike/app.js
+++ jumpy-mike/app.js
@@ -43,53 +43,6 @@ function init() {
 	G.ui.area.style.backgroundColor='#DDD';
 	G.ui.pts = {}
 
-	G.music={tempo:111}
-	G.music.ac = typeof AudioContext !== 'undefined' ? new AudioContext : new webkitAudioContext,
-	lead = [
-		'-   e','Bb3 e','A3  e','Bb3 e','G3  e','A3  e','F3  e','G3  e',
-		'E3  e','F3  e','G3  e','F3  e','E3  e','F3  e','D3  q',
-		'-   e','Bb3 s','A3  s','Bb3 e','G3  e','A3  e','G3  e','F3  e','G3  e',
-		'E3  e','F3  e','G3  e','F3  e','E3  s','F3  s','E3  e','D3  q'
-	],
-	harmony = [
-		'-   e','D4  e','C4  e','D4  e','Bb3 e','C4  e','A3  e','Bb3 e',
-		'G3  e','A3  e','Bb3 e','A3  e','G3  e','A3  e','F3  q',
-		'-   e','D4  s','C4  s','D4  e','Bb3 e','C4  e','Bb3 e','A3  e','Bb3 e',
-		'G3  e','A3  e','Bb3 e','A3  e','G3  s','A3  s','G3  e','F3  q'
-	],
-	bass = [
-		'D3  q','-   h','D3  q',
-		'A2  q','-   h','A2  q',
-		'Bb2 q','-   h','Bb2 q',
-		'F2  h','A2  h'
-	];
-	G.music.seq1 = new TinyMusic.Sequence( G.music.ac, G.music.tempo, lead );
-	G.music.seq2 = new TinyMusic.Sequence( G.music.ac, G.music.tempo, harmony );
-	G.music.seq3 = new TinyMusic.Sequence( G.music.ac, G.music.tempo, bass );
-
-	// set staccato and smoothing values for maximum coolness
-	G.music.seq1.staccato = 0.55;
-	G.music.seq2.staccato = 0.55;
-	G.music.seq3.staccato = 0.05;
-	G.music.seq3.smoothing = 0.4;
-
-	// adjust the levels so the bass and harmony aren't too loud
-	G.music.seq1.gain.gain.value = 1.0 / 2;
-	G.music.seq2.gain.gain.value = 0.8 / 2;
-	G.music.seq3.gain.gain.value = 0.65 / 2;
-
-	// apply EQ settings
-	G.music.seq1.mid.frequency.value = 800;
-	G.music.seq1.mid.gain.value = 3;
-	G.music.seq2.mid.frequency.value = 1200;
-	G.music.seq3.mid.gain.value = 3;
-	G.music.seq3.bass.gain.value = 6;
-	G.music.seq3.bass.frequency.value = 80;
-	G.music.seq3.mid.gain.value = -6;
-	G.music.seq3.mid.frequency.value = 500;
-	G.music.seq3.treble.gain.value = -2;
-	G.music.seq3.treble.frequency.value = 1400;
-	
 	G.restart();
 
 }//FILE: sprites.js 
@@ -282,6 +235,7 @@ G.restart = function() {
 		y:0
 	}
 	G.player.image.onload = G.start;
+	G.music.restart();
 }//FILE: game.js 
 //FILE: loop.js
 G.update = function() {
@@ -310,7 +264,7 @@ G.update = function() {
 		var nextCactus = (Math.random()*G.spacing)+40;
 		//dp(G.ticks, G.ticks-G.lastCactus, nextCactus,G.ticks-G.lastCactus>nextCactus)
 		if (G.ticks-G.lastCactus>nextCactus) {
-			console.log('spacing:',G.spacing,'last:', G.ticks-G.lastCactus,'next:',nextCactus, G.ticks-G.lastCactus>nextCactus)
+			dpd('spacing:',G.spacing,'last:', G.ticks-G.lastCactus,'next:',nextCactus, G.ticks-G.lastCactus>nextCactus)
 			var p=Math.random();
 			G.addCactus(0,Math.round(Math.random()))
 			if(G.level>1 && p>0.75) G.addCactus(10,Math.round(Math.random()))
@@ -395,18 +349,13 @@ G.addCactus = function(x,t) {
 }
 G.start = function() {
 	G._intervalId = requestAnimationFrame(G.loop);//setInterval(G.loop, 1000/G.ui.fps);
-	var when = G.music.ac.currentTime;
-	G.music.seq1.play( when );
-	G.music.seq2.play( when + ( 60 / G.music.tempo ) * 16 );// delay the harmony by 16 beats
-	G.music.seq3.play( when );
+	G.music.play();
 }
 G.pause = function() {
 	if (G._intervalId) {
 		clearInterval(G._intervalId);
 		delete G._intervalId;
-		G.music.seq1.stop();
-		G.music.seq2.stop();
-		G.music.seq3.stop();
+		G.music.stop();
 	} else G.start();
 }
 

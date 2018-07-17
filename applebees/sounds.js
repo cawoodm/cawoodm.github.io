e0134d81d27450a2676476d0b5992b98590355fc
Marc <498834+cawoodm@users.noreply.github.com>
2018-07-18 00:41:59 +0200

    Icon

--- applebees/sounds.js
+++ applebees/sounds.js
@@ -4,7 +4,11 @@ g.sounds = {
     ,lose: new Audio('./resources/lose.mp3')
 }
 g.sounds.ping.volume = 0.5;
+g.sounds.ping.loop = false;
+
 g.sounds.music = g.sounds.music1;
 g.sounds.music.volume = 0.5;
 g.sounds.music.loop = true;
+
 g.sounds.lose.volume = 0.5;
+g.sounds.lose.loop = false;

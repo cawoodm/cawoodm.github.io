36231c9321b71a272e25f937c8d582ead9cd7ee6
Marc <498834+cawoodm@users.noreply.github.com>
2018-07-19 02:39:11 +0200

    Remove debug

--- applebees/bullet.js
+++ applebees/bullet.js
@@ -2,13 +2,15 @@ function Bullet(options) {
 	this.x = options.x || 0;
 	this.y = options.y || 0;
 	this.tag = "bullet";
+	this.collider=25;
+	this.velocity=2;
     this.speed = {x: options.speed.x, y: options.speed.y};
     this.img = Images.get.bullet;
     return this;
 }
 Bullet.prototype.update = function(delta) {
-    this.x += this.speed.x * 25 * delta;
-    this.y += this.speed.y * 25 * delta;
+    this.x += this.speed.x * this.velocity * delta;
+    this.y += this.speed.y * this.velocity * delta;
 }
 Bullet.prototype.renderer = function(ctx) {
     ctx.drawImage(this.img, this.x, this.y);
